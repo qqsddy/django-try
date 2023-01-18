be78d7bb4623016088dc78e31e28a99f3fa0a426
anitachen <yian.chen1204@gmail.com>
2023-01-18 13:50:46 +0800

    updated views and prod

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 import os
 from .base import *
-from google.oauth2 import service_account
+#from google.oauth2 import service_account
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
@@ -16,9 +16,9 @@ STATIC_ROOT = '/demo/www/public'
 
 ## For media store in the bucket
 ## Getting credential
-GS_CREDENTIALS = service_account.Credentials.from_service_account_file(os.path.join(BASE_DIR, 'credential.json'))
+#GS_CREDENTIALS = service_account.Credentials.from_service_account_file(os.path.join(BASE_DIR, 'credential.json'))
 
 ##configuration for media file storing and retriving media file from gcloud
-DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = 'try-deploy-django-bucket'
-MEDIA_URL = 'https://storage.googleapis.com/try-deploy-django-bucket/'
+#DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+#GS_BUCKET_NAME = 'try-deploy-django-bucket'
+#MEDIA_URL = 'https://storage.googleapis.com/try-deploy-django-bucket/'
